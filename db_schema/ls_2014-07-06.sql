87551498ef9a03b373bc40df5e49c53d0e2ac78f
Jerry Jiang <leeming-jiang@gmail.com>
2014-07-08 16:25:16 +0800

    upgrade db schema

--- db_schema/ls_2014-07-06.sql
+++ db_schema/ls_2014-07-06.sql
@@ -1,107 +1,149 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4096
-#
-# http://www.sequelpro.com/
-# http://code.google.com/p/sequel-pro/
-#
-# Host: 127.0.0.1 (MySQL 5.6.13)
-# Database: ls
-# Generation Time: 2014-07-06 12:18:41 +0000
-# ************************************************************
-
+CREATE DATABASE  IF NOT EXISTS `ls` /*!40100 DEFAULT CHARACTER SET gb2312 */;
+USE `ls`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: localhost    Database: ls
+-- ------------------------------------------------------
+-- Server version	5.6.17
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
-# Dump of table ls_city
-# ------------------------------------------------------------
+--
+-- Table structure for table `ls_city`
+--
 
 DROP TABLE IF EXISTS `ls_city`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ls_city` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL DEFAULT '',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
   `province_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+  PRIMARY KEY (`id`),
+  KEY `FK1AC7A1236659F8AB` (`province_id`),
+  CONSTRAINT `FK1AC7A1236659F8AB` FOREIGN KEY (`province_id`) REFERENCES `ls_province` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=gb2312;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
-
-# Dump of table ls_city_url
-# ------------------------------------------------------------
+--
+-- Table structure for table `ls_city_url`
+--
 
 DROP TABLE IF EXISTS `ls_city_url`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ls_city_url` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
   `type` varchar(255) DEFAULT NULL,
-  `url` varchar(500) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+) ENGINE=InnoDB DEFAULT CHARSET=gb2312;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
-
-# Dump of table ls_company
-# ------------------------------------------------------------
+--
+-- Table structure for table `ls_company`
+--
 
 DROP TABLE IF EXISTS `ls_company`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ls_company` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
   `contactor` varchar(100) DEFAULT NULL,
-  `email` varchar(11) DEFAULT NULL,
-  `phone` varchar(20) DEFAULT NULL,
-  `address` varchar(600) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
   `email_src` varchar(200) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
   `phone_src` varchar(200) DEFAULT NULL,
   `is_tracked` tinyint(1) DEFAULT NULL,
+  `address` varchar(600) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=gbk;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-# Dump of table ls_company_resource
-# ------------------------------------------------------------
+--
+-- Table structure for table `ls_company_problem`
+--
+
+DROP TABLE IF EXISTS `ls_company_problem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ls_company_problem` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_id` int(11) NOT NULL,
+  `problem_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK6A2D4E258B150249` (`problem_id`),
+  CONSTRAINT `FK6A2D4E258B150249` FOREIGN KEY (`problem_id`) REFERENCES `ls_problem` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=gb2312;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `ls_company_resource`
+--
 
 DROP TABLE IF EXISTS `ls_company_resource`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ls_company_resource` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `url` varchar(500) DEFAULT NULL,
-  `name` varchar(200) DEFAULT NULL,
-  `type` varchar(50) DEFAULT NULL,
-  `grab_count` int(11) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `grab_by` int(11) DEFAULT NULL,
+  `grab_count` int(11) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+) ENGINE=InnoDB DEFAULT CHARSET=gb2312;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `ls_company_step`
+--
 
+DROP TABLE IF EXISTS `ls_company_step`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ls_company_step` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `company_id` int(11) NOT NULL,
+  `step_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK370E9906D7B923AB` (`step_id`),
+  CONSTRAINT `FK370E9906D7B923AB` FOREIGN KEY (`step_id`) REFERENCES `ls_step` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=gb2312;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-# Dump of table ls_group
-# ------------------------------------------------------------
+--
+-- Table structure for table `ls_group`
+--
 
 DROP TABLE IF EXISTS `ls_group`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ls_group` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(300) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
-
-# Dump of table ls_job
-# ------------------------------------------------------------
+--
+-- Table structure for table `ls_job`
+--
 
 DROP TABLE IF EXISTS `ls_job`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ls_job` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(200) DEFAULT NULL,
@@ -110,55 +152,87 @@ CREATE TABLE `ls_job` (
   `grab_url` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
-
-# Dump of table ls_job_log
-# ------------------------------------------------------------
+--
+-- Table structure for table `ls_job_log`
+--
 
 DROP TABLE IF EXISTS `ls_job_log`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ls_job_log` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL COMMENT 'start by user',
   `job_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
-
-# Dump of table ls_location
-# ------------------------------------------------------------
+--
+-- Table structure for table `ls_location`
+--
 
 DROP TABLE IF EXISTS `ls_location`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ls_location` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `ls_problem`
+--
 
+DROP TABLE IF EXISTS `ls_problem`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ls_problem` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=gb2312;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-# Dump of table ls_province
-# ------------------------------------------------------------
+--
+-- Table structure for table `ls_province`
+--
 
 DROP TABLE IF EXISTS `ls_province`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ls_province` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL DEFAULT '',
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+) ENGINE=InnoDB DEFAULT CHARSET=gb2312;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `ls_step`
+--
 
+DROP TABLE IF EXISTS `ls_step`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ls_step` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=gb2312;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-# Dump of table ls_track_log
-# ------------------------------------------------------------
+--
+-- Table structure for table `ls_track_log`
+--
 
 DROP TABLE IF EXISTS `ls_track_log`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ls_track_log` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) DEFAULT NULL,
@@ -167,14 +241,15 @@ CREATE TABLE `ls_track_log` (
   `comments` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-
-
-# Dump of table ls_user
-# ------------------------------------------------------------
+--
+-- Table structure for table `ls_user`
+--
 
 DROP TABLE IF EXISTS `ls_user`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ls_user` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(100) CHARACTER SET utf8 NOT NULL DEFAULT '',
@@ -184,50 +259,52 @@ CREATE TABLE `ls_user` (
   UNIQUE KEY `name` (`name`),
   KEY `username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-# Dump of table ls_user_location
-# ------------------------------------------------------------
+--
+-- Table structure for table `ls_user_location`
+--
 
 DROP TABLE IF EXISTS `ls_user_location`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `ls_user_location` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,ls_city
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned NOT NULL,
-  `city_id` int(11) unsigned NOT NULL,
-  `location_id` int(11) NOT NULL,
+  `location_id` int(11) unsigned NOT NULL,
+  `city_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FKCCBB20F17DC122B5` (`user_id`),
-  KEY `city_id` (`city_id`),
-  CONSTRAINT `ls_user_location_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `ls_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `ls_user_location_ibfk_2` FOREIGN KEY (`city_id`) REFERENCES `ls_city` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+  KEY `FKCCBB20F169835B21` (`location_id`),
+  CONSTRAINT `FKCCBB20F169835B21` FOREIGN KEY (`location_id`) REFERENCES `ls_user` (`id`),
+  CONSTRAINT `FKCCBB20F17DC122B5` FOREIGN KEY (`user_id`) REFERENCES `ls_location` (`id`),
+  CONSTRAINT `ls_user_location_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `ls_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `t_user`
+--
 
-
-DROP TABLE IF EXISTS `ls_problem`;
+DROP TABLE IF EXISTS `t_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ls_problem` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=gbk;
-
-DROP TABLE IF EXISTS `ls_company_problem`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `ls_company_problem` (
-  `id` int(11) NOT NULL,
-  `company_id` int(11) NOT NULL,
-  `problem_id` int(11) NOT NULL,
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account` varchar(25) NOT NULL,
+  `password` varchar(25) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK6A2D4E258B150249` (`problem_id`),
-  CONSTRAINT `FK6A2D4E258B150249` FOREIGN KEY (`problem_id`) REFERENCES `ls_problem` (`id`)
+  UNIQUE KEY `account` (`account`)
 ) ENGINE=InnoDB DEFAULT CHARSET=gbk;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-07-08 16:23:38
